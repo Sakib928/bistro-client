68f955af57404a4fee922d3205a2f91a8dfd1ded
Sakib928 <sakibgscian@gmail.com>
2024-05-17 10:15:34 +0600

    created initial files

---
+++ src/Layout/Main.jsx
@@ -0,0 +1,13 @@
+import { Outlet } from "react-router-dom";
+import Footer from "../shared/Footer/Footer";
+
+const Main = () => {
+  return (
+    <>
+      <Outlet></Outlet>
+      <Footer></Footer>
+    </>
+  );
+};
+
+export default Main;
